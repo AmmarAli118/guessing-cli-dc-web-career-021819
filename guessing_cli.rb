3704b4a32dce3d73229eacb8792d0618f8520e08
Ammar Ali <funnyguy118@gmail.com>
2019-02-06 18:21:21 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   input = ""
   while input
     puts "Guess a number between 1 and 6."
-    input = gets.downcase.chomp
+    input = gets.chomp
     random_number = rand(1..6).to_s
     case input
     when random_number
